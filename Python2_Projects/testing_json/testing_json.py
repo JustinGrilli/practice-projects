625cad571d8646f6dcf73462d4afab1ac194971a
justin grilli <justin.grilli@gmail.com>
2018-10-18 16:00:09 -0700

    added stuff

--- Python2_Projects/testing_json/testing_json.py
+++ Python2_Projects/testing_json/testing_json.py
@@ -3,14 +3,29 @@ from pprint import pprint
 
 
 manowar_data_json = open('results_manowar.json', 'r')
-manowar_data_json = manowar_data_json.read().split('\n')
-# print manowar_data_json
-for x in manowar_data_json:
-    results_manowar_json = json.loads(x)
-    pprint(results_manowar_json)
+manowar_data_json_dict_list = manowar_data_json.read().split('\n')
+manowar_data_json_dict_list = filter(None, manowar_data_json_dict_list)
+
+classes_list = []
+for x in manowar_data_json_dict_list:
+    current_dict = json.loads(x)
+    if current_dict['class'] not in classes_list:
+        classes_list.append(current_dict['class'])
+
+print '\nClasses:\n'
+pprint(classes_list)
+print '\n\n_____________________________________________________________________________________________________________\n'
+for x in manowar_data_json_dict_list:
+    current_dict = json.loads(x)
+    if current_dict['class'] == 'BuildingVersion':
+        pprint(current_dict)
+        pprint('_____________________________________________________________________________________________________________')
 
 
-json_file = open('data_file.json', 'r')
-data_results = json.load(json_file)
+# pprint(json.loads(manowar_data_json))
+
+
+# json_file = open('data_file.json', 'r')
+# data_results = json.load(json_file)
 #
 # print data_results
