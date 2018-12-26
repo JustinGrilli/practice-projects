aafd379933d42c1ba1e2d8d0d2712f030ff2e9d7
Justango <justin.grilli@gmail.com>
2018-12-25 20:07:39 -0800

    did tv show routing in the same function as movie routing... seemed more efficient

--- Python3_Projects/Organize_Media.py
+++ Python3_Projects/Organize_Media.py
@@ -5,40 +5,6 @@ import json
 import re
 
 
-def flatten_tv_show_folders(download_path, delete_folders=True):
-    for file in os.listdir(download_path):
-        subpath = os.path.join(download_path, file)
-        tv_show_folder = re.findall(r's\d+e\d+', file.lower())
-        if tv_show_folder != [] and os.path.isdir(subpath):
-            for subfile in os.listdir(subpath):
-                tv_show_file = re.findall(r's\d+e\d+', subfile.lower())
-                if tv_show_file != [] and os.path.isfile(os.path.join(subpath, subfile)) and subfile.split('.')[-1] in media_extentions:
-                    shutil.move(os.path.join(subpath, subfile), os.path.join(download_path, subfile))
-            if delete_folders:
-                shutil.rmtree(subpath)
-    return None
-
-
-def organize_tv_shows(download_path, media_path):
-    for file in os.listdir(download_path):
-        file_path = os.path.join(download_path, file)
-        renamed_file = initcap_file_name(file)
-        tv_show_episode = re.findall(r'[sS]\d+[eE]\d+', renamed_file)
-        if tv_show_episode != [] and os.path.isfile(file_path) and renamed_file.split('.')[-1] in media_extentions:
-            renamed_file = renamed_file.split(tv_show_episode[0])[0]+tv_show_episode[0]+'.'+renamed_file.split('.')[-1]
-            season = int(re.sub(r'[^0-9]', '', tv_show_episode[0].lower().split('e')[0]))
-            media_show_path = os.path.join(media_path, 'TV Shows', renamed_file.split(' '+tv_show_episode[0])[0], 'Season '+str(season))
-            moved_file = os.path.join(media_show_path, file)
-            if os.path.exists(media_show_path):
-                shutil.move(file_path, moved_file)
-                os.rename(moved_file, os.path.join(media_show_path, renamed_file))
-            else:
-                os.makedirs(media_show_path)
-                shutil.move(file_path, moved_file)
-                os.rename(moved_file, os.path.join(media_show_path, renamed_file))
-    return None
-
-
 def initcap_file_name(string):
     words = re.sub(r'[^a-zA-Z0-9()]', ' ', string).split(' ')
     extension = words[-1]
@@ -48,7 +14,7 @@ def initcap_file_name(string):
     return new_string
 
 
-def recursively_organize_movies(dl_path, media_path, delete_folders=True):
+def recursively_organize_shows_and_movies(dl_path, media_path, delete_folders=True):
     movies_folder = os.path.join(media_path, 'Movies')
     folders_in_main = [folders for path, folders, files in os.walk(dl_path) if path == dl_path][:][0]
     folders_to_delete = []
@@ -58,7 +24,23 @@ def recursively_organize_movies(dl_path, media_path, delete_folders=True):
             for file in files:
                 movies_file_path = os.path.join(movies_folder, file)
                 current_file_path = os.path.join(path, file)
-                if file.split('.')[-1] in media_extentions and os.path.isfile(current_file_path):
+                tv_show_episode = re.findall(r'[sS]\d+[eE]\d+', initcap_file_name(file))
+                # Route for TV Shows
+                if tv_show_episode != [] and file.split('.')[-1] in media_extentions and os.path.isfile(current_file_path):
+                    season = int(re.sub(r'[^0-9]', '', tv_show_episode[0].lower().split('e')[0]))
+                    show_folder = os.path.join(media_path, 'TV Shows', initcap_file_name(file).split(' ' + tv_show_episode[0])[0], 'Season ' + str(season))
+                    show_file_path = os.path.join(show_folder, file)
+                    renamed_file = initcap_file_name(file)
+                    renamed_file = renamed_file.split(tv_show_episode[0])[0]+tv_show_episode[0]+'.'+renamed_file.split('.')[-1]
+                    if os.path.exists(show_folder):
+                        shutil.move(current_file_path, show_file_path)
+                        os.rename(show_file_path, os.path.join(show_folder, renamed_file))
+                    else:
+                        os.makedirs(show_folder)
+                        shutil.move(current_file_path, show_file_path)
+                        os.rename(show_file_path, os.path.join(show_folder, renamed_file))
+                # Route for Movies
+                elif file.split('.')[-1] in media_extentions and os.path.isfile(current_file_path):
                     if os.path.exists(movies_folder):
                         shutil.move(current_file_path, movies_file_path)
                         os.rename(movies_file_path, os.path.join(movies_folder, initcap_file_name(file)))
@@ -71,7 +53,26 @@ def recursively_organize_movies(dl_path, media_path, delete_folders=True):
             for file in files:
                 movies_file_path = os.path.join(movies_folder, file)
                 current_file_path = os.path.join(path, file)
-                if file.split('.')[-1] in media_extentions and os.path.isfile(current_file_path):
+                tv_show_episode = re.findall(r'[sS]\d+[eE]\d+', initcap_file_name(file))
+                # Route for TV Shows
+                if tv_show_episode != [] and file.split('.')[-1] in media_extentions and os.path.isfile(current_file_path):
+                    season = int(re.sub(r'[^0-9]', '', tv_show_episode[0].lower().split('e')[0]))
+                    show_folder = os.path.join(media_path, 'TV Shows', initcap_file_name(file).split(' ' + tv_show_episode[0])[0], 'Season ' + str(season))
+                    show_file_path = os.path.join(show_folder, file)
+                    renamed_file = initcap_file_name(file)
+                    renamed_file = renamed_file.split(tv_show_episode[0])[0]+tv_show_episode[0]+'.'+renamed_file.split('.')[-1]
+                    if path not in folders_to_delete:
+                        folders_to_delete.append(path)
+
+                    if os.path.exists(show_folder):
+                        shutil.move(current_file_path, show_file_path)
+                        os.rename(show_file_path, os.path.join(show_folder, renamed_file))
+                    else:
+                        os.makedirs(show_folder)
+                        shutil.move(current_file_path, show_file_path)
+                        os.rename(show_file_path, os.path.join(show_folder, renamed_file))
+                # Route for Movies
+                elif file.split('.')[-1] in media_extentions and os.path.isfile(current_file_path):
                     if path not in folders_to_delete:
                         folders_to_delete.append(path)
 
@@ -113,7 +114,5 @@ finally:
     paths = saves.read()
     dl_path = json.loads(paths)['downloads']
     m_path = json.loads(paths)['media']
-    flatten_tv_show_folders(dl_path, delete_folders=True)
-    organize_tv_shows(dl_path, m_path)
-    recursively_organize_movies(dl_path, m_path, delete_folders=True)
+    recursively_organize_shows_and_movies(dl_path, m_path, delete_folders=True)
     saves.close()
