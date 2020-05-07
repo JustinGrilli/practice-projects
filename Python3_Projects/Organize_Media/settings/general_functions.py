54b73477f14b0655d8d18d121babbdba2d84278b
=JustinG <=justin.grilli@gmail.com>
2020-05-07 09:10:49 -0700

    refactored and fixed logic for organize_media program

--- Python3_Projects/Organize_Media/settings/general_functions.py
+++ Python3_Projects/Organize_Media/settings/general_functions.py
@@ -1,5 +1,7 @@
-import re, os
-from mo_settings import media_extensions
+import re
+import os
+from datetime import date
+from .mo_settings import media_extensions
 
 
 def initcap_file_name(string):
@@ -19,41 +21,51 @@ def tv_show_ep(file):
     :return: The tv show pattern and tv show season number.
     """
     bad_nums = ['360', '480', '720', '1080', '264']
+    tv_show_episode = []
     season = []
-    # if the tv show has a pattern like s01e01
-    tv_show_episode = re.findall(r'[sS]\d+[eE]\d+', initcap_file_name(file))
-    if tv_show_episode != []:
-        tv_show_episode = tv_show_episode[0]
-        season = int(re.sub(r'[^0-9]', '', tv_show_episode.lower().split('e')[0]))
-    else:
-        # if the tv show has a pattern like 1x01
-        tv_show_episode = re.findall(r'\d+[xX]\d+', initcap_file_name(file))
-        if tv_show_episode != []:
-            tv_show_episode = tv_show_episode[0]
-            season = int(re.sub(r'[^0-9]', '', tv_show_episode.lower().split('x')[0]))
-        else:
-            # if the tv show has a pattern like 101
-            numbers = re.findall(r'\d+', initcap_file_name(file))
-            for num in numbers:
-                if (len(num) == 3 and num not in bad_nums) or \
-                        (len(num) == 4 and num[:2] != '20' and int(num[-2:]) < 30 and num not in bad_nums):
-                    tv_show_episode.append(num)
-            if tv_show_episode != []:
-                tv_show_episode = tv_show_episode[0]
-                season = int(re.sub(r'[^0-9]', '', tv_show_episode[:-2]))
+    # if the tv show has a pattern like...
+    patterns = [r'[sS]\d{1,2}[eE]\d{1,2}',  # s1e1 or s01e01
+                r'\d{1,2}[xX]\d{1,2}',  # 1x01 or 10x01
+                r'[._\- ](\d{3,4})[._\- ]'  # 101 or 1001
+                ]
+    current_year = date.today().year
+    for pattern in patterns:
+        matches = re.findall(pattern, file)
+        # If the pattern returns matches
+        if matches:
+            if patterns.index(pattern) != 2:
+                tv_show_episode = matches[0]
+                season = int(re.findall(r'\d+', tv_show_episode)[0])
+            else:
+                matches = [num for num in matches
+                           if int(num[-2:]) < 30 and num not in bad_nums
+                           and (int(num) < current_year-30 or int(num) > current_year)]
+                if matches:
+                    tv_show_episode = matches[0]
+                    season = int(re.sub(r'[^0-9]', '', tv_show_episode[:-2]))
+            continue
 
     return tv_show_episode, season
 
 
 def rename_all_media_in_directory(media_path, media_extensions=media_extensions):
+    """ Renames all of the media files in the path
+
+    :param media_path: Path to the files to rename
+    :param media_extensions: A list of acceptable media extentions
+    :return: None
+    """
     for path, folders, files in os.walk(media_path):
         for file in files:
             file_path = os.path.join(path, file)
             tv_show_episode, season = tv_show_ep(file)
-            if tv_show_episode != [] and file.split('.')[-1] in media_extensions and os.path.isfile(file_path):
-                renamed_file = initcap_file_name(file)
-                renamed_file = renamed_file.split(tv_show_episode)[0] + tv_show_episode + '.' + renamed_file.split('.')[-1]
+            renamed_file = initcap_file_name(file)
+            # If it is a media file
+            if file.split('.')[-1] in media_extensions and os.path.isfile(file_path):
+                # If it's a TV show episode
+                if tv_show_episode != []:
+                    # Remove extract crap after the episode in name -- i.e. family.guy.s01e01.XXX420JUSTBLAZE.crap.mp4
+                    renamed_file = renamed_file.split(tv_show_episode)[0] + tv_show_episode + '.' + renamed_file.split('.')[-1]
                 os.rename(file_path, os.path.join(path, renamed_file))
-            elif file.split('.')[-1] in media_extensions and os.path.isfile(file_path):
-                os.rename(file_path, os.path.join(path, initcap_file_name(file)))
+    return None
 
--- Python3_Projects/Organize_Media/settings/mo_functions.py
+++ Python3_Projects/Organize_Media/settings/mo_functions.py
@@ -1,5 +1,7 @@
-import tkinter.filedialog as filedialog, json
-from mo_settings import required_paths
+import tkinter.filedialog as filedialog
+import json
+import os
+from .mo_settings import required_paths
 
 
 def save_paths_to_json(json_file_name='settings', required_paths=required_paths):
@@ -13,18 +15,14 @@ def save_paths_to_json(json_file_name='settings', required_paths=required_paths)
 
 
 def get_downloads_or_media_path(path='downloads'):
-    try:
-        # If the locations are already saved to a file, open that file
-        saves = open('settings/settings.json', 'r')
-    except FileNotFoundError:
-        # If the locations are not saved, ask for the locations and save them to a  json file
+    if not os.path.exists('settings/settings.json'):
+        # If the locations are not saved, ask for the locations and save them to a json file
         save_paths_to_json(json_file_name='settings', required_paths=required_paths)
-        # Now open that file
-        saves = open('settings/settings.json', 'r')
-    finally:
-        # Load json file
-        paths_dict = json.load(saves)
-        saves.close()
+
+    # Load json file
+    saves = open('settings/settings.json', 'r')
+    paths_dict = json.load(saves)
+    saves.close()
 
     return paths_dict[path]
 
