e49de93fa045211e067515db5ba79e1cde3a2c8d
=JustinG <=justin.grilli@gmail.com>
2020-05-07 19:18:37 -0700

    update

--- Python3_Projects/Organize_Media/settings/mo_functions.py
+++ Python3_Projects/Organize_Media/settings/mo_functions.py
@@ -1,34 +1,32 @@
 import tkinter.filedialog as filedialog
 import json
-import os
 
 
-with open('settings/config.json', 'r') as config:
-    configuration = json.load(config)
-    required_paths = configuration['required_paths']
+def save_paths_to_json():
 
+    with open('settings/config.json', 'r') as c:
+        config = json.load(c)
+        required_paths = config['required_paths']
 
-def save_paths_to_json(json_file_name='settings', required_paths=required_paths):
-    dictionary = {}
     for item in required_paths:
-        path = filedialog.askdirectory(title=f'Path to {item} folder')
-        dictionary[item] = path
+        path = filedialog.askdirectory(title=f'Path to {item.title()} folder')
+        config[item] = path
 
-    with open(f'settings/{json_file_name}.json', 'w') as saved_location:
-        json.dump(dictionary, saved_location)
+    with open('settings/config.json', 'w') as n:
+        json.dump(config, n, indent=2)
+
+    return config
 
 
 def get_downloads_or_media_path(path='downloads'):
-    if not os.path.exists('settings/settings.json'):
-        # If the locations are not saved, ask for the locations and save them to a json file
-        save_paths_to_json(json_file_name='settings', required_paths=required_paths)
 
-    # Load json file
-    saves = open('settings/settings.json', 'r')
-    paths_dict = json.load(saves)
-    saves.close()
+    with open('settings/config.json', 'r') as c:
+        config = json.load(c)
+        if path not in config:
+            # If the locations are not saved, ask for the locations and save them to a json file
+            config = save_paths_to_json()
 
-    return paths_dict[path]
+    return config[path]
 
 
 
